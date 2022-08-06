9c409338ad4c6b83f988e9057163b1a7731d0c0c
Ehsanur Rahman Rhythm <ehsanur.rahman.rhythm@g.bracu.ac.bd>
2022-08-06 21:39:22 +0600

    Added First Page

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+$site_name = "My Site";
+$css_path = "css/main.css";
+$db_host = "localhost";
+$db_user = "root";
+$db_pass = "";
+$db_name = "my_site";
+$css_cmd = "npx tailwindcss -i ./css/style.css -o ./css/main.css --watch";
+?>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,13 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
-  content: ["css/*.css"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+	content: ["*.{php, html, js}", "*/*.{php, html, js}"],
+	theme: {
+		extend: {},
+	},
+	plugins: [require("@tailwindcss/typography"), require("daisyui")],
+	daisyui: {
+		theme: "light",
+		darkTheme: "corporate",
+	},
 };
